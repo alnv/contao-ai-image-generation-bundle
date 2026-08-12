75155c166f65d91cce55c1f5cd129e8f4834d06d
Alexander Naumov <alexandernaumov@MacBookPro.fritz.box>
2026-08-12 10:38:00 +0200

    fx

--- src/Resources/contao/dca/tl_gemini_options.php
+++ src/Resources/contao/dca/tl_gemini_options.php
@@ -2,6 +2,7 @@
 
 use Doctrine\DBAL\Platforms\AbstractMySQLPlatform;
 use Contao\DC_Table;
+use Contao\DataContainer;
 
 $GLOBALS['TL_DCA']['tl_gemini_options'] = [
     'config' => [
@@ -30,7 +31,10 @@ $GLOBALS['TL_DCA']['tl_gemini_options'] = [
         ],
     ],
     'palettes' => [
-        'default' => 'name;model,aspectRatio;prompt;references'
+        '__selector__' => ['type'],
+        'default' => 'type',
+        'image' => 'type,name;model,aspectRatio;prompt;references',
+        'video' => 'type,subType,name;model;prompt;references'
     ],
     'fields' => [
         'id' => [
@@ -38,6 +42,19 @@ $GLOBALS['TL_DCA']['tl_gemini_options'] = [
         ],
         'tstamp' => [
             'sql' => ['type' => 'integer', 'notnull' => false, 'unsigned' => true, 'default' => 0]
+        ],
+        'type' => [
+            'inputType' => 'select',
+            'eval' => [
+                'maxlength' => 16,
+                'tl_class' => 'w50',
+                'mandatory' => true,
+                'submitOnChange' => true
+            ],
+            'filter' => true,
+            'reference' => &$GLOBALS['TL_LANG']['tl_gemini_options'],
+            'options' => ['video', 'image'],
+            'sql' => ['type' => 'string', 'length' => 16, 'default' => 'image']
         ],
         'name' => [
             'inputType' => 'text',
@@ -58,7 +75,42 @@ $GLOBALS['TL_DCA']['tl_gemini_options'] = [
                 'mandatory' => true
             ],
             'filter' => true,
-            'options' => ['gemini-2.5-flash-image', 'gemini-3.1-flash-image-preview'],
+            'options_callback' => function (DataContainer $dc) {
+                $type = $dc->getCurrentRecord()['type'];
+                if ($type == 'image') {
+                    return ['gemini-2.5-flash-image', 'gemini-3.1-flash-image-preview', 'gemini-3.1-flash-image'];
+                }
+
+                if ($type == 'video') {
+                    return ['veo-3.1-generate-preview'];
+                }
+
+                return [];
+            },
+            'sql' => ['type' => 'string', 'length' => 32, 'default' => '']
+        ],
+        'subType' => [
+            'inputType' => 'select',
+            'eval' => [
+                'maxlength' => 32,
+                'tl_class' => 'w50',
+                'mandatory' => true,
+                'includeBlankOption' => true
+            ],
+            'filter' => true,
+            'options_callback' => function (DataContainer $dc) {
+                $type = $dc->getCurrentRecord()['type'];
+                /*
+                if ($type == 'image') {
+                    return ['image-to-image'];
+                }
+                */
+                if ($type == 'video') {
+                    return ['image-to-video', 'images-to-image', 'frames-to-image'];
+                }
+
+                return [];
+            },
             'sql' => ['type' => 'string', 'length' => 32, 'default' => '']
         ],
         'aspectRatio' => [
--- src/Resources/contao/languages/de/tl_gemini_options.php
+++ src/Resources/contao/languages/de/tl_gemini_options.php
@@ -5,3 +5,7 @@ $GLOBALS['TL_LANG']['tl_gemini_options']['model'] = ['Gemini-Modell', ''];
 $GLOBALS['TL_LANG']['tl_gemini_options']['prompt'] = ['Prompt', ''];
 $GLOBALS['TL_LANG']['tl_gemini_options']['references'] = ['Zusätzliche Bilder', ''];
 $GLOBALS['TL_LANG']['tl_gemini_options']['aspectRatio'] = ['Ratio', ''];
+$GLOBALS['TL_LANG']['tl_gemini_options']['type'] = ['Typ', ''];
+$GLOBALS['TL_LANG']['tl_gemini_options']['subType'] = ['Kategorie', ''];
+$GLOBALS['TL_LANG']['tl_gemini_options']['video'] = ['Video', ''];
+$GLOBALS['TL_LANG']['tl_gemini_options']['image'] = ['Bild', ''];
